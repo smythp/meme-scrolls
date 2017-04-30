0c54b60da1dcb54e5fe206b7939062502c34f878
Patrick Smyth <patricksmyth01@gmail.com>
2017-04-30 12:13:11 -0400

    initial commit

---
+++ meme.py
@@ -0,0 +1,71 @@
+while True:
+    print("The Meme Scrolls [type play]")    
+    my_input = input()
+    if my_input == "play":
+        break
+    else:
+        print ("TYPE PLAY Y0U DUMBO! DID YOU NOT READ THE INSTRUCTI0NS?!?!?!")
+    
+print ("0nce in a land where there are dragons and fairies and other mythical stuff you were born and your mama kicked you out of the house and said go on an adventure and you did. you packed our meme scrolls, wings and cheetos, and other stuff.")
+
+
+
+print("You eventually are at the forest of magically wierd stoof. You  look at the trees . They look very tree-like. you wonder, where are the dragons? type one  or two to see what happens next!")
+
+answer=input()
+if answer=="one":
+    print("A dragons pops out at you. You take out the meme scrolls and show a Pokemon meme! The dragon becomes your friend. you name it Bulbasaur cuz its green and you showed it a pokemon meme.")
+elif answer=="two":
+    print("No dragons show up. you keep on walking for now.")
+else:
+    print("1 0R 2 0KAY?!?!?!") 
+
+print ("You are out in the forest, and a group of evil fairies show up. What do you do? Type one or two.")
+
+answer=input()
+if answer=="one":
+	print("You get eaten by the fairies and your mother doesnt care.")
+	exit()
+elif answer=="two":
+	print("You take out a doge scroll and the fairies get knocked out. You pick up a scroll from them. it is... a JOHN CENA scroll!")
+else:
+	print("ONE OR TWO OKAY?!!!")
+
+
+print("you come across a cave. You slowly start walking in, and a dragon pops up! What do you do? Type One OR Two!")
+answer=input()
+
+if answer=="one":
+	print("You take out  the JOHN CENA scroll you got from the fairies. The dragon becomes your friend! You name it skimpi.")
+elif answer=="two":
+	print("The dragon eats you before you get a chance to pull out one of your scrolls.")
+	exit()
+else:
+	print("ONE OR TWO! YOU SHOULD KNOW DAT!") 
+	
+print("You approach the end of the tunnel, and you see an elf. The elf's name is Bean. Bean is currently a dragon tamer using meme scrolls, just like you! Do you attack, or do you befriend Type One OR Two to find out!")
+
+answer=input()
+if answer=="one":
+	print("You become friends with Bean. Bean currently has two dragons, just like you! eyyyyyyy")
+elif answer=="two":
+	print("Bean becomes aggresive and makes you a dragon with a Markiplier enchantment. The End")
+	exit()
+else:
+	print("ONE OR TWO!")
+	
+print("You and Bean come across Slenderman. You dash to find the paper towels before you die. What happens? Type One or Two to find out!")
+
+answer=input()
+
+if answer=="one":
+	print("You find seven of the paper towels until Slenderman comes up to you and gouges your memeness out and you die. The End")
+	exit()
+elif answer=="two":
+	print("You and Bean split up to try to find the paper towels. You find four, and Bean finds four. You show the towels to Slenderman. He's almost finished off! You take out Bulbasaur, and KABLOOIE! Slenderman dies! He drops twenty YouTube viewers, which are really rare. You and Bean share them. EYYYYY")
+	
+else:
+    print("SAY ONE OR TWO DUMBO!")
+	
+print("After defeating slenderman, you and bean return to town. You are declared heroes! Seven days are spent celebrating your memeness. Deep in the forest, there is another force that will need to be conqured... a mysterious thing called a movie.")
+print("THE END... UNTIL THE MEME SCROLLS II -- THE ENCHANTMENT OF THE MOO-VIE")
